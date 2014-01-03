f436e9ccf36b8fe98fb51c86840f54bf5714d594
Vinícius <vinicius.dutra@gmail.com>
2014-01-03 20:42:15 -0200

    Adicionado campos de busca. Ajustes no layout webpy

--- README.md
+++ README.md
@@ -4,14 +4,19 @@ pub
 Projeto de um simples CRUD de veiculos.
 
 
-Requisitos:
+<h1>Requisitos:</h1>
 
-MongoDB 2.0.4
-Python 2.7.3
-
-
+- MongoDB 2.0.4
+- Python 2.7.3
 
 
-TESTE inicial
 
-O script python inicializa\_db\_carscrud.py inicializa um banco de dados e collection como dados demo no mongodb
+
+<h1>TESTE inicial</h1>
+
+<h2>Inicializar base DEMO</h2>
+<p>O script python inicializa\_db\_carscrud.py inicializa um banco de dados e collection como dados demo no mongodb</p>
+
+<h2>Rodar App</h2>
+<p>Executar o app que irá servir o app web:</p>
+> __main__.py
--- __main__.py
+++ __main__.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python
 import pymongo
 import web
 from web import form
 
 
-render = web.template.render('templates/')
+render = web.template.render('templates/', base='layout')
 
 urls = (
 	'/', 'Index',
@@ -12,10 +13,20 @@ urls = (
 
 app = web.application(urls, globals())
 
+#formulario de login da area adm do crud
 login = form.Form(
     form.Textbox('Username'),
     form.Password('Password'),
     form.Button('Login'),
+    validators = [form.Validator("Password error", lambda i: i.Username == i.Password)]
+)
+
+#formulario de busca do CRUD
+search = form.Form(
+    form.Textbox('Fabricante'),
+    form.Textbox('Modelo'),
+    form.Textbox('Ano'),
+    form.Button('Buscar'),
     validators = [form.Validator("Password error", lambda i: i.Username == i.Password)]
 )
 
@@ -37,8 +48,10 @@ class Index:
 	cars = []
 	for i in cars_list:
 	    cars.append(dict([(str(k), str(v)) for k, v in i.items()]))
-	print cars
-        return render.index(cars)
+	#debug
+	#print cars
+	search_fields = search()
+        return render.index(search_fields,cars)
 
 
 class Crud:
