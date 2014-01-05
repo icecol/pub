4ed1d18631e9a71607f398f9dc4fae26651cb5fd
Vinícius <vinicius.dutra@gmail.com>
2014-01-05 11:51:21 -0200

    Ajustes da busca

--- __main__.py
+++ __main__.py
@@ -27,7 +27,7 @@ search = form.Form(
     form.Textbox('Modelo'),
     form.Textbox('Ano'),
     form.Button('Buscar'),
-    validators = [form.Validator("Password error", lambda i: i.Username == i.Password)]
+    validators = [form.Validator("Ano incorreto", lambda x: x.Ano < 9999)]
 )
 
 
@@ -37,28 +37,37 @@ class Index:
 	client = pymongo.MongoClient('localhost', 27017)
 	#define a base utilizada
 	db = client.carscrud
-        """ Show page """
+	#busca registros na base e coloca em lista
         posts = db.cars.find()
 	cars_list = []
 	for i in posts:
 	    cars_list.append(i)
-	print cars_list
-
-	# "decodifica unicode"
+	# "decodifica" unicode retornado pelo mongodb
 	cars = []
 	for i in cars_list:
 	    cars.append(dict([(str(k), str(v)) for k, v in i.items()]))
-	#debug
-	#print cars
 	search_fields = search()
         return render.index(search_fields,cars)
+
+    def POST(self):
+	cars = []
+	search_fields = search()
+	if search_fields.validates():
+	    return render.index(search_fields,cars)
+	fabricante = search_fields['Fabricante'].value
+	modelo = search_fields['Modelo'].value
+	ano = search_fields['Ano'].value
+	print(fabricante)
+	print(modelo)
+	print(ano)
+	return render.index(search_fields,cars)
 
 
 class Crud:
     def GET(self):
 	form = login()
 	print form.render()
-        return render.formtest(form)
+        return render.login(form)
 
     def POST(self): 
         form = login() 
